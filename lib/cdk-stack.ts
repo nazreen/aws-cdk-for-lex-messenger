61bf82e43759cf750778d755916426e546a95487
Nazreen Mohamad <nxmohamad@gmail.com>
2019-08-19 19:53:01 +0800

    limit actions to lex, set props

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -12,6 +12,7 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const ACCOUNT_ID = cdk.Stack.of(this).account
+    const STACK_NAME = cdk.Stack.of(this).stackName
 
     /*
       Setup IAM Permissions
@@ -19,7 +20,7 @@ export class CdkStack extends cdk.Stack {
     const lexPolicyStatement = new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
       resources: [`arn:aws:lex:${LEX_REGION}:${ACCOUNT_ID}:bot:*:*`],
-      actions: ['lex:PostText']
+      actions: ['lex:*'] // TODO: specify to only PostText and create separate Role for the lambda custom resource provider
     })
     const policyDocument = new iam.PolicyDocument()
     policyDocument.addStatements(lexPolicyStatement)
@@ -61,9 +62,10 @@ export class CdkStack extends cdk.Stack {
     /*
      Custom Resource
     */
-    const resource = new MyCustomResource(this, 'DemoResource', {
-      message: 'CustomResource says hello',
-    });
+    const propsForLambdaProvider = {
+      stackName: STACK_NAME
+    };
+    const resource = new MyCustomResource(this, 'custom-resource-lex-bot', propsForLambdaProvider, webhookHandlerRole);
 
     // Publish the custom resource output
     new cdk.CfnOutput(this, 'ResponseMessage', {
