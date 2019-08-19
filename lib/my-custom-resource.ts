dd20a0ffe59714180202574703adbf20457185b0
Nazreen Mohamad <nxmohamad@gmail.com>
2019-08-19 19:48:25 +0800

    require role

--- lib/my-custom-resource.ts
+++ lib/my-custom-resource.ts
@@ -1,5 +1,6 @@
 import cfn = require('@aws-cdk/aws-cloudformation');
 import lambda = require('@aws-cdk/aws-lambda');
+import iam = require('@aws-cdk/aws-iam');
 import cdk = require('@aws-cdk/core');
 
 import fs = require('fs');
@@ -14,18 +15,20 @@ export interface MyCustomResourceProps {
 export class MyCustomResource extends cdk.Construct {
     public readonly response: string;
 
-    constructor(scope: cdk.Construct, id: string, props: MyCustomResourceProps) {
+    constructor(scope: cdk.Construct, id: string, props: MyCustomResourceProps, role: iam.Role) {
         super(scope, id);
 
         const resource = new cfn.CustomResource(this, 'Resource', {
-            provider: cfn.CustomResourceProvider.lambda(new lambda.SingletonFunction(this, 'Singleton', {
+            provider: cfn.CustomResourceProvider.lambda(new lambda.SingletonFunction(this, 'custom-resource-provider', {
                 uuid: 'f7d4f730-4ee1-11e8-9c2d-fa7ae01bbebc',
                 code: new lambda.InlineCode(fs.readFileSync('lib/custom-resource-handler.py', { encoding: 'utf-8' })),
                 handler: 'index.main',
-                timeout: cdk.Duration.seconds(300),
+                timeout: cdk.Duration.seconds(10),
                 runtime: lambda.Runtime.PYTHON_3_7,
+                role
             })),
-            properties: props
+            properties: props,
+            resourceType: 'Custom::LexBot'
         });
 
         this.response = resource.getAtt('Response').toString();
